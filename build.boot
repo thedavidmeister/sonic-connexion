fb49b16d1044246eec6b8a72e7cd3fc6f8d11321
David Meister <thedavidmeister@gmail.com>
2018-05-06 18:36:27 +1000

    environ support for twitter keys

--- build.boot
+++ build.boot
@@ -5,7 +5,8 @@
    [samestep/boot-refresh "0.1.0-20160620.111804-1"]
    [http-kit "2.3.0"]
    [cheshire "5.8.0"]
-   [com.taoensso/timbre "4.10.0"]]
+   [com.taoensso/timbre "4.10.0"]
+   [environ "1.1.0"]]
 
  :source-paths   #{"src"})
 
--- src/twitter/api.clj
+++ src/twitter/api.clj
@@ -5,18 +5,23 @@
   cheshire.core
   taoensso.timbre
   twitter.spec
+  environ.core
   [clojure.spec.alpha :as spec]))
 
 (defn -fetch-token!
- [client-key client-secret]
- {:post [(spec/valid? :twitter.token/token %)]}
- (let [response @(org.httpkit.client/post
-                  twitter.data/token-url
-                  {:basic-auth [client-key client-secret]
-                   :form-params {"grant_type" "client_credentials"}})]
-  (if (= 200 (:status response))
-   (cheshire.core/parse-string
-    (:body response)
-    true)
-   (taoensso.timbre/error (:status response) (:body response)))))
+ ([]
+  (-fetch-token!
+   (environ.core/env :twitter-key)
+   (environ.core/env :twitter-secret)))
+ ([client-key client-secret]
+  {:post [(spec/valid? :twitter.token/token %)]}
+  (let [response @(org.httpkit.client/post
+                   twitter.data/token-url
+                   {:basic-auth [client-key client-secret]
+                    :form-params {"grant_type" "client_credentials"}})]
+   (if (= 200 (:status response))
+    (cheshire.core/parse-string
+     (:body response)
+     true)
+    (taoensso.timbre/error (:status response) (:body response))))))
 (def fetch-token! (memoize -fetch-token!))
